586a8fa256108caa245061783745b006bb76d31c
Igor <igor.olshanskyi@gmail.com>
2025-02-25 15:52:39 +0200

    test_1

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
+//import { extend } from 'cypress/types/lodash';
 import React from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+interface Props {
+  message: string;
+}
+
+export class App extends React.Component<{}, Props> {
+  state: Props = {
+    message: 'Nothing was pressed yet',
+  };
+
+  handle = (event: KeyboardEvent) => {
+    this.setState({
+      message: `The last pressed key is [${event.key}]`,
+    });
+  };
+
+  componentDidMount(): void {
+    document.addEventListener('keyup', this.handle);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <p className="App__message">{this.state.message}</p>
+      </div>
+    );
+  }
+}
